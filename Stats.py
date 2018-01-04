7e8a5417880600f3371c7985df771f03328de20e
qoutsie <github@qoutsy.33mail.com>
2018-01-04 06:42:02 +0200

    Stats.py shows statistics!
    WeW ^^

---
+++ Stats.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import os
+import platform
+statsos = str(platform.system())
+statscpu = (platform.processor())
+keyboard.send_keys("\n**CPU:** " + statscpu + "\n**OS:** " + statsos + "\n" + "\n "  + "\n")
